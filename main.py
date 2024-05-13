4428c11eab73997c096e549f9dd7709efb467952
filipeparreira <filipeaugusto@alunos.utfpr.edu.br>
2024-05-13 17:21:56 -0300

    file: add main.py 'requisitos para o programa'

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+    Criar um sistema bancario com as operacoes de saque, deposito e extrato
+
+    Deposito: 
+        - Deve ser possivel depositar valores positivos 
+        - Como essa versao contem somente 1 usuario n eh necessario identificar agencia ou conta
+        - Todos os depositos devem ser armazenados em uma variavel 
+        - Todos os depositos dever ser exibidos na operacao de extrato
+    
+    Saque: 
+        - Deve permitir realizar 3 saques diarios com limite de 500 reais por saque
+        - Caso o usuario nao tenha saldo em conta, exibir uma mensagem informando que nao foi possivel realizar
+            o saque por falta de dinheiro.
+        - Todos os saques devem ser armazenados em uma variavel para serem exibidos no extrato
+    
+    Extrato:
+        - Listar todos os depositos e saques da conta 
+        - No fim da listagem deve exibir o saldo atual da conta
+        - Os valores devem ser exibidos no formato R$ xxx.xx
+'''
